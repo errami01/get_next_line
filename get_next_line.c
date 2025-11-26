879a986862494e9fef07b689dda611c6ef29233a
Abdellatif Errami <aberrami@c1r1s5.42prague.com>
2025-11-26 19:07:03 +0100

    Add test to check how read() works

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
+#include "get_next_line.h"
+
+// char	*get_next_line(int fd)
+// {
+// 	char buffer[255];
+// 	char 
+// }
+#include "stdio.h"
+#include <fcntl.h>
+int main()
 {
-		
+	char b[100];
+	int i = 0;
+	int fd = open("test.txt", O_RDONLY);
+	if(fd == -1)
+		printf("Error opening the file");
+	read(fd, b, sizeof(b));
+	printf("result: %s\n", b);
+	while (*b)
+		i++;
+	printf("%i", i);
+
 }
